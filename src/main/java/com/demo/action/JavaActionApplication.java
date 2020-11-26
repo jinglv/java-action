aa5d24aa301cce24b21b422866853d4e2ebe64ff
吕晶048807 <lvjing@renmaitech.com>
2020-11-26 14:02:50 +0800

    feat:新增traceId实现日志跟踪

--- src/main/java/com/demo/action/JavaActionApplication.java
+++ src/main/java/com/demo/action/JavaActionApplication.java
@@ -2,12 +2,14 @@ package com.demo.action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author jingLv
  * @date 2020/11/19
  */
 @SpringBootApplication
+@ServletComponentScan
 public class JavaActionApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/demo/action/filter/TraceIdFilter.java
@@ -0,0 +1,48 @@
+package com.demo.action.filter;
+
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * TraceId过滤器
+ *
+ * @author jingLv
+ * @date 2020/11/26
+ */
+@WebFilter(urlPatterns = "/*")
+@Order(1)
+public class TraceIdFilter implements Filter {
+    /**
+     * TraceId常量
+     */
+    private static final String TRACE_ID = "traceId";
+
+    /**
+     * 为当前线程放入一个trace_id
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //尝试从请求信息中获取traceId信息
+        String traceId = servletRequest.getParameter(TRACE_ID);
+        // 为空设置默认值
+        if (StringUtils.isEmpty(traceId)) {
+            traceId = UUID.randomUUID().toString();
+        }
+        // 在MDC中放入traceId
+        MDC.put(TRACE_ID, traceId);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
